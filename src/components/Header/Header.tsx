67e022e78951c7b5916b04b86eabfc4c040a10c0
MarcosRidley <marcosridley@hotmail.com>
2022-06-18 16:31:54 -0300

    add 768 media query breakpoint

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,11 +11,21 @@ export default function Header({setItemFilterName}: HeaderProps): JSX.Element {
 	const { pathname } = useLocation();
 	return (
 		<header>
-				<Link to={'/'} className='logo'><img src={logo} alt="mercadolivre logo" /> </Link>
-				<h3 className='store-name'>Ridley's fast store</h3>
-				<Searchbar setItemFilterName={setItemFilterName} />
+			<Link to={'/'} className="logo">
+				<img src={logo} alt="mercadolivre logo" />{' '}
+			</Link>
+			<a
+				href="https://www.linkedin.com/in/marcosridley/"
+				className="store-name"
+				style={{textDecoration: 'none', color: '#555', fontWeight: 'bold'}}
+			>
+				Ridley's store
+			</a>
+			<Searchbar setItemFilterName={setItemFilterName} />
 			<Link to={pathname === '/' ? '/cart' : '/'}>
-				<button className="page-switch-btn">{pathname === '/' ? 'Carrinho' : 'Home'}</button>
+				<button className="page-switch-btn">
+					{pathname === '/' ? 'Carrinho' : 'Home'}
+				</button>
 			</Link>
 		</header>
 	);
